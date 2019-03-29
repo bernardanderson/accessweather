a440d5a8cda39e1fbfd6f828a7d66833efee7620
banderso <bernard.anderson@gmail.com>
2019-03-28 23:14:08 -0500

    Created weatherData initial state

--- frontend/src/initialState.js
+++ frontend/src/initialState.js
@@ -1,15 +1,23 @@
 import PropTypes from 'prop-types';
 
 export const initialPropState = {
-    simpleReducer: {
-        crazyBool: false
+    currentWeatherData: {
+        baromin: 0,
+        dailyrainin: 0,
+        dewptf: 0,
+        humidity: 0,
+        rainin: 0,
+        tempf: 0,
+        time: 0,
+        winddir: 0,
+        windgustdir: 0,
+        windgustmph: 0,
+        windspeedmph: 0
     }
 };
 
 export const initialPropType = {
-    simpleReducer: PropTypes.shape({
-        crazyBool: PropTypes.bool
-    })
+    currentWeatherData: PropTypes.objectOf(PropTypes.number)
 };
 
 export default initialPropState;
