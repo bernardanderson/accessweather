77fa0d073ec8c898694257cd49699d564d4abdb3
banderso <bernard.anderson@gmail.com>
2019-04-09 22:42:56 -0500

    Updated SocketIoClass in BE for console.logging

--- backend/Services/SocketIoService.js
+++ backend/Services/SocketIoService.js
@@ -6,15 +6,13 @@ let startServer = function() {
     console.log("Socket.io server starting");
 
     io.on('connection', function(socket) {
-        console.log("Connection established...");
+        console.log("A connection to a Client is established...");
 
         socket.on('getCurrentWeatherData', function() {
-            console.log("Asking for current weather data")
-            const currentData = weatherDataService.getTheCurrentWeatherData();
-            socket.emit('currentWeatherData', currentData);
+            console.log("A Client is asking for most recent weather data")
+            socket.emit('currentWeatherData', weatherDataService.getTheCurrentWeatherData());
         });
     })
-    
 
    io.listen(7024);
 }
