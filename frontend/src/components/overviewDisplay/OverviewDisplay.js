6f76c2cd560fc1d732b729e33bf2226cd76b4c34
banderso <bernard.anderson@gmail.com>
2019-04-11 23:32:53 -0500

    Added semantic ui to FE project

--- frontend/src/components/overviewDisplay/OverviewDisplay.js
+++ frontend/src/components/overviewDisplay/OverviewDisplay.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import * as weatherActions from '../../actions/weatherAction';
-import initialPropState, { initialPropType } from '../../initialState';
+import { initialPropState, initialPropType } from '../../initialState';
 import PropTypes from 'prop-types';
 import './OverviewDisplay.scss';
 import ioSvc from '../../services/SocketIoService';
@@ -16,17 +15,41 @@ class OverviewDisplay extends Component {
     static defaultProps = {
         ...initialPropState
     };
-    
-    constructor(props, context) {
-        super(props, context);
-    }
 
     render() {
+
+        const tempHumDew = {
+            Temperature: <div>{this.props.currentWeatherData.tempf}<span className="smaller-font">°F</span></div>,
+            Humidity: <div>{this.props.currentWeatherData.humidity}<span className="smaller-font">%</span></div>,
+            Dewpoint: <div>{this.props.currentWeatherData.dewptf}<span className="smaller-font">°F</span></div>
+        };
+
         return (
         <div className = "App">
-            <pre>
-                {`${this.props.currentWeatherData.baromin}`}
-            </pre>
+            <div className="ui equal width grid overview-display">
+                <div className="column">
+                    { 
+                        Object.keys(tempHumDew).map(key => 
+                            <div key={key} className="ui segment">
+                                    <h1 className="ui center aligned header">
+                                        {key}
+                                    </h1>
+                                <div className="larger-font">
+                                    <div className="ui huge center aligned header">
+                                        {tempHumDew[key]}
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+                <div className="eight wide column">
+                    <div className="ui segment">
+                    </div>
+                </div>
+                <div className="column">
+                </div>
+            </div>
         </div>
         );
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import 'semantic-ui/dist/semantic.css';
 import './index.scss';
 import OverviewDisplay from './components/overviewDisplay/OverviewDisplay';
 import * as serviceWorker from './serviceWorker';
