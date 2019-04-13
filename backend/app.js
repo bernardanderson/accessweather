0d1e2507f875299dc3eb341e415dd250fa585672
banderso <bernard.anderson@gmail.com>
2019-04-13 11:47:13 -0500

    Removed all CORS stuff from backend

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,16 @@
 var express = require("express");
 var socketIo  = require("./Services/SocketIoService.js");
-var cors = require('cors');
 var weatherDataService = require("./Services/WeatherDataService.js");
 const config = require('./config.js');
 var app = express();
-var http = require("http").Server(app);
 const applicationPort = config.app.port;
-
-app.use(cors());
 
 app.get('/weatherstation/updateweatherstation.php', (req, res) => {
     let dbInsertResult = weatherDataService.insertWeatherDataIntoDb(req.query);
-
     socketIo.io.sockets.emit('currentWeatherData', weatherDataService.getTheCurrentWeatherData());
-
     return (dbInsertResult) ? res.status(200).send() : res.status(400).send();
 });
 
-// app.get('/weatherdata/temperatures/getmonthlyhighslows', (req, res) => {
-//     let tempChartData = weatherDataService.getDailyTemperatureHighsAndLows();
-//     res.status(200).send(tempChartData);
-// });
-
 app.use('/', express.static('public'));
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
-http.listen(applicationPort, () => console.log(`The AccuriteAccess Weather Data Application has started on port ${applicationPort}`));
+app.listen(applicationPort, () => console.log(`The AccuriteAccess Weather Data Application has started on port ${applicationPort}`));
